2762987572f736b84c7e6fd41a6a640555baa70d
Poorva Patil <poorvapatil@Poorvas-MacBook-Pro.local>
2020-02-04 16:24:33 -0500

    d

--- starter-hw04/length-sort.c
+++ starter-hw04/length-sort.c
@@ -8,12 +8,37 @@ sort(svec* xs)
 {
     // TODO: sort input vector by string length
     // see "man 3 strlen"
+
+    // GNOME SORT WIKI PSEUDOCODE
+    // pos := 0
+    // while pos < length(a):
+    //     if (pos == 0 or a[pos] >= a[pos-1]):
+    //         pos := pos + 1
+    //     else:
+    //         swap a[pos] and a[pos-1]
+    //         pos := pos - 1
+
+    int pos=0;
+    while(pos < xs->size){
+        if (pos == 0 || strlen(xs->data[pos]) >= strlen(xs->data[pos-1])){
+            pos+=1;
+        }
+        else{
+            svec_swap(&xs, pos, pos-1);
+            pos-=1;
+        }
+    } 
+
+
 }
 
 void
 chomp(char* text)
 {
     // TODO: Modify input string to remove trailing newline ('\n')
+    int l=strlen(text);
+    if( text[l-1] == '\n' )
+        text[l-1] = 0;
 }
 
 int
