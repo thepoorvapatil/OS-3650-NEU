d9f0684a074e3a4626b47c27829280e4b35b5f97
thepoorvapatil <45820209+thepoorvapatil@users.noreply.github.com>
2020-02-10 11:50:52 -0500

    Add files via upload

---
+++ starter-hw05/tokens.c
@@ -0,0 +1,20 @@
+
+/* TODO:
+
+   while (1) {
+     printf("tokens$ ");
+     fflush(stdout);
+     line = read_line()
+     if (that was EOF) {
+        exit(0);
+     }
+     tokens = tokenize(line);
+     foreach token in reverse(tokens):
+       puts(token)
+   }
+
+*/
+
+
+
+
