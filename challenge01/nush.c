5525487d519ac4c76b4465e8ef40856d1e60990f
Poorva Patil <poorvapatil@Poorvas-MacBook-Pro.local>
2020-02-18 16:42:36 -0500

    isas nush comment rtrn

--- challenge01/nush.c
+++ challenge01/nush.c
@@ -6,25 +6,25 @@
 #include <sys/wait.h>
 #include <ctype.h>
 #include <errno.h>
+#include "parse.h"
 #include "svec.h"
 #include "ast.h"
-#include "parse.h"
 
 int
-isOperator(const char* text, int ii){
+isOperator(const char* str, int ii){
 	
-	if(text[ii] == '<' || text[ii] == '>' || text[ii] == ';')
+	if(str[ii] == '<' || str[ii] == '>' || str[ii] == ';')
 		return 1;
 
-	if(text[ii] == '|'){
-		if(text[ii+1] == '|')
+	if(str[ii] == '|'){
+		if(str[ii+1] == '|')
 			return 2;
 		else 
 			return 1;
 	}
 
-	if(text[ii] == '&'){
-		if(text[ii+1] == '&')
+	if(str[ii] == '&'){
+		if(str[ii+1] == '&')
 			return 2;
 		else 
 			return 1;
@@ -36,14 +36,14 @@ isOperator(const char* text, int ii){
 
 
 char*
-read_token(const char* text, int ii){
+read_token(const char* str, int ii){
 	int nn = 0;
 
-	while (!isspace(text[ii+nn]) && isOperator(text, ii+nn) == 0)
+	while (!isspace(str[ii+nn]) && isOperator(str, ii+nn) == 0)
 		++nn;
 
 	char* token = malloc (nn + 1);
-	memcpy(token, text + ii, nn);
+	memcpy(token, str + ii, nn);
 	token[nn] = 0;
 	
 	return token; 
@@ -90,52 +90,32 @@ tokenize (const char* text) {
 void
 execute(shell_ast* ast)
 {
-	//Checking for built in commands.
 	if( ast->isOperator = 0 ){
 
 		if( strcmp(ast->cmd, "cd") == 0){
-		//Change directory. 
-		chdir(svec_get(ast->args, 0));
+            //built in function to cd
+            chdir(svec_get(ast->args, 0));
 		}
-
 		if(strcmp(ast->cmd, "exit") == 0) {
-		//Exit built in.
-		exit(0);
+		    exit(0);
 		}
 
 	} else {
-    		int cpid;
+    	int cpid;
+        if ((cpid = fork())) {
+            // Parent process.
 
-    		if ((cpid = fork())) {
-        		// Parent process.
-	
-        		//printf("Parent pid: %d\n", getpid());
-        		//printf("Parent knows child pid: %d\n", cpid);
+            int status;
+            waitpid(cpid, &status, 0);
 
-        		// Child may still be running until we wait.
-
-        		int status;
-        		waitpid(cpid, &status, 0);
-
-        		//printf("== executed program complete ==\n");
-
-        		//printf("child returned with wait code %d\n", status);
-        		if (WIFEXITED(status)) {
-            		//printf("child exited with exit code (or main returned) %d\n", WEXITSTATUS(status));
-        		}
-   		 } else {
-        		//Child process.
-        		//printf("Child pid: %d\n", getpid());
-        		//printf("Child knows parent pid: %d\n", getppid());
-
-	
-        		//printf("== executed program's output: ==\n");
-	
-			//Evaluating the abstract sintax tree to execute the command. 
-        		ast_eval(ast);
-
-        		//printf("Can't get here, exec only returns on error.\n");
-    		}
+            // if (WIFEXITED(status)) {
+            // 	printf("child exited with exit code (or main returned) %d\n", WEXITSTATUS(status));
+            // }
+        } 
+        else {
+            //Child process.
+            ast_eval(ast);
+        }
 	}
 }
 
@@ -162,57 +142,45 @@ main(int argc, char* argv[])
     		char cmd[256];
 
     		if (argc == 1) {
-		//No script file introduced.
 
 			while(1) {
 
-				//Prompting.
-        			printf("nush$ ");
-
+        		printf("nush$ ");
 				fflush(stdout);
-			
-				//Reading command.
-        			char* rtrn = fgets(cmd, 256, stdin);
-				if (!rtrn){
-					//Breaking in case user wants to end. 
-					printf("\n");
-					break;
-				}
+			    fgets(cmd, 256, stdin);
 
+				// if (!rtrn){
+				// 	//Breaking in case user wants to end. 
+				// 	printf("\n");
+				// 	break;
+				// }
 
 				if(strlen(cmd) == 1){
-					//Nothing introduced. 
 					continue;
 				}
 
-				//Processing input.
 				processInput(cmd);
 			}
 
-    		} else {
-		//Script file introduced. 
-			
-			//Reading from the input file and storing it in buffer. 
-			
-			//Creating buffer for lines and opening file. 
+    		} 
+            else {
+                //file input
+            
         		char buffer[100];
-			FILE* input = fopen(argv[1], "r");
-			//Error check in fopen
-			if (input == NULL){
+			    FILE* f = fopen(argv[1], "r");
+			    //Error check in fopen
+			    if (f == NULL){
 				printf("Error in fopen %s\n", strerror(errno));
 				exit(0);
 			}
 
-			//While there is a line to read
-			while (fgets(buffer, 100, input) != NULL){
-				//We process the command that each line repesents.
+			while (fgets(buffer, 100, f) != NULL){
 				processInput(buffer);
-			}	
+			}
 			
-			//Closing file. 
-			fclose(input);
+			//Close file 
+			fclose(f);
 			
-
     		}		
 	
     return 0;
