cc7a9ebcdb9d5c40045acde7991f80aa3686c935
Poorva Patil <poorvapatil@Poorvas-MacBook-Pro.local>
2020-02-18 16:33:55 -0500

    isa's nush

--- challenge01/nush.c
+++ challenge01/nush.c
@@ -4,74 +4,76 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <ctype.h>
 #include <errno.h>
+#include "svec.h"
 #include "ast.h"
 #include "parse.h"
-#include "svec.h"
 
 int
-isOperator(const char* str, int ii){
+isOperator(const char* text, int ii){
 	
-	if(str[ii] == '<' || str[ii] == '>' || str[ii] == ';')
+	if(text[ii] == '<' || text[ii] == '>' || text[ii] == ';')
 		return 1;
 
-    //check for ||
-	if(str[ii] == '|'){
-		if(str[ii+1] == '|')
+	if(text[ii] == '|'){
+		if(text[ii+1] == '|')
 			return 2;
 		else 
 			return 1;
 	}
 
-    //check for &&
-	if(str[ii] == '&'){
-		if(str[ii+1] == '&')
+	if(text[ii] == '&'){
+		if(text[ii+1] == '&')
 			return 2;
 		else 
 			return 1;
 	}
+	
+
 	return 0; 
 }
 
+
 char*
-read_token(const char* str, int ii){
+read_token(const char* text, int ii){
 	int nn = 0;
 
-	while (!isspace(str[ii+nn]) && isOperator(str, ii+nn) == 0)
+	while (!isspace(text[ii+nn]) && isOperator(text, ii+nn) == 0)
 		++nn;
 
 	char* token = malloc (nn + 1);
-	memcpy(token, str + ii, nn);
+	memcpy(token, text + ii, nn);
 	token[nn] = 0;
 	
 	return token; 
 }
 
 svec* 
-tokenize (const char* str) {
+tokenize (const char* text) {
 	
 	svec* vector = make_svec();
-	int nn = strlen(str);
+	int nn = strlen(text);
 	int ii = 0;
 	char *token;
 
 	while (ii < nn) {
 	
-		if(isspace(str[ii])){
+		if(isspace(text[ii])){
 			ii++; //if it is an space we skip it. 
 
 		} else {
-			int opLength = isOperator(str, ii);
+			int opLength = isOperator(text, ii);
 
 			if (opLength == 0) {
 				//it is not an operator
-				token = read_token(str, ii);
+				token = read_token(text, ii);
 				svec_push_back(vector, token);
 				ii += strlen(token);	
 			} else {
 				//it is an operator
 				token = malloc(opLength + 1);
-				memcpy(token, str + ii, opLength);
+				memcpy(token, text + ii, opLength);
 				token[opLength] = 0;
 				svec_push_back(vector, token);
 				ii += opLength;		
@@ -88,130 +90,130 @@ tokenize (const char* str) {
 void
 execute(shell_ast* ast)
 {
-    if( ast->isOperator = 0 ){
+	//Checking for built in commands.
+	if( ast->isOperator = 0 ){
 
 		if( strcmp(ast->cmd, "cd") == 0){
-		//inbuilt cd
+		//Change directory. 
 		chdir(svec_get(ast->args, 0));
 		}
 
 		if(strcmp(ast->cmd, "exit") == 0) {
+		//Exit built in.
 		exit(0);
 		}
-    }
-    else{
-        int cpid;
 
-        if ((cpid = fork())) {
-            // parent process
-            printf("Parent pid: %d\n", getpid());
-            printf("Parent knows child pid: %d\n", cpid);
+	} else {
+    		int cpid;
 
-            // Child may still be running until we wait.
+    		if ((cpid = fork())) {
+        		// Parent process.
+	
+        		//printf("Parent pid: %d\n", getpid());
+        		//printf("Parent knows child pid: %d\n", cpid);
 
-            int status;
-            waitpid(cpid, &status, 0);
+        		// Child may still be running until we wait.
 
-            printf("== executed program complete ==\n");
+        		int status;
+        		waitpid(cpid, &status, 0);
 
-            printf("child returned with wait code %d\n", status);
-            if (WIFEXITED(status)) {
-                printf("child exited with exit code (or main returned) %d\n", WEXITSTATUS(status));
-            }
-        }
-        else {
-            // child process
+        		//printf("== executed program complete ==\n");
 
-            ast_eval(ast);
-            // printf("Child pid: %d\n", getpid());
-            // printf("Child knows parent pid: %d\n", getppid());
+        		//printf("child returned with wait code %d\n", status);
+        		if (WIFEXITED(status)) {
+            		//printf("child exited with exit code (or main returned) %d\n", WEXITSTATUS(status));
+        		}
+   		 } else {
+        		//Child process.
+        		//printf("Child pid: %d\n", getpid());
+        		//printf("Child knows parent pid: %d\n", getppid());
 
-            // for (int ii = 0; ii < strlen(cmd); ++ii) {
-            //     if (cmd[ii] == ' ') {
-            //         cmd[ii] = 0;
-            //         break;
-            //     }
-            // }
+	
+        		//printf("== executed program's output: ==\n");
+	
+			//Evaluating the abstract sintax tree to execute the command. 
+        		ast_eval(ast);
 
-            // // The argv array for the child.
-            // // Terminated by a null pointer.
-            // char* args[] = {cmd, "one", 0};
-
-            // printf("== executed program's output: ==\n");
-
-            // execvp(cmd, args);
-            // printf("Can't get here, exec only returns on error.");
-        }
-    }
+        		//printf("Can't get here, exec only returns on error.\n");
+    		}
+	}
 }
+
+void processInput(char* cmd){
+	
+	//Converting command froms tring to resizable array of strings.
+	svec *vector = tokenize(cmd);
+
+	//Converting the array of strings into an Abstract Sintax Tree.
+	shell_ast* ast = parse(vector);
+	
+	//Freeing array of strings because we don't need it anymore. 
+	free(vector);
+	
+	//Executing command. 
+	execute(ast);
+}
+
 
 int
 main(int argc, char* argv[])
 {
-    char cmd[256];
 
-    if (argc == 1) {
-        while (1)
-        {
-            printf("nush$ ");
-            fflush(stdout);
-            fgets(cmd, 256, stdin);
+    		char cmd[256];
 
-            if (!cmd){
-                printf("\n");
-                break;
-            }
+    		if (argc == 1) {
+		//No script file introduced.
 
-            if(strlen(cmd)==1){
-                continue;
-            }
+			while(1) {
 
-            svec *vector = tokenize(cmd);
+				//Prompting.
+        			printf("nush$ ");
 
-            //Converting the array of strings into an Abstract Sintax Tree.
-            shell_ast* ast = parse(vector);
-            
-            //Freeing array of strings because we don't need it anymore. 
-            free(vector);
-            
-            //Executing command. 
-            execute(ast);
-        }
-        
-        
-    }
-    //read from file given as argument.
-    else {
-        // memcpy(cmd, "echo", 5);
-        char buffer[100];
-        FILE* f = fopen(argv[1], "r");
-        //check for file error
-        if (f == NULL){
-			printf("Error in fopen %s\n", strerror(errno));
-			exit(0);
-		}
-        //read  file
-
-        while (fgets(buffer, 100, f) != NULL){
-            svec *vector = tokenize(cmd);
-
-            //Converting the array of strings into an Abstract Sintax Tree.
-            shell_ast* ast = parse(vector);
-            
-            //Freeing array of strings because we don't need it anymore. 
-            free(vector);
-            
-            //Executing command. 
-            execute(ast);
+				fflush(stdout);
 			
-		}	
+				//Reading command.
+        			char* rtrn = fgets(cmd, 256, stdin);
+				if (!rtrn){
+					//Breaking in case user wants to end. 
+					printf("\n");
+					break;
+				}
 
-        //close file
-        fclose(f);
 
-    }
+				if(strlen(cmd) == 1){
+					//Nothing introduced. 
+					continue;
+				}
 
-    execute(cmd);
+				//Processing input.
+				processInput(cmd);
+			}
 
+    		} else {
+		//Script file introduced. 
+			
+			//Reading from the input file and storing it in buffer. 
+			
+			//Creating buffer for lines and opening file. 
+        		char buffer[100];
+			FILE* input = fopen(argv[1], "r");
+			//Error check in fopen
+			if (input == NULL){
+				printf("Error in fopen %s\n", strerror(errno));
+				exit(0);
+			}
+
+			//While there is a line to read
+			while (fgets(buffer, 100, input) != NULL){
+				//We process the command that each line repesents.
+				processInput(buffer);
+			}	
+			
+			//Closing file. 
+			fclose(input);
+			
+
+    		}		
+	
     return 0;
 }
