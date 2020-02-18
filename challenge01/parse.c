131218a3b67b9f526399f887696a7dbfe84a3c74
Poorva Patil <poorvapatil@Poorvas-MacBook-Pro.local>
2020-02-18 17:04:33 -0500

    undo isa parse.c

--- challenge01/parse.c
+++ challenge01/parse.c
@@ -12,9 +12,10 @@ streq(const char* aa, const char* bb){
 	return strcmp(aa,bb) == 0;
 }
 
-int find_first_index(svec* toks, const char* tt){
+int 
+find_first_index(svec* toks, const char* tt){
 
-		for (int ii=0; ii < toks->size; ++ii){
+		for (int ii=0; ii < toks->size; ii++){
 			char* token = svec_get(toks, ii);
 			if( streq(token, tt) ){
 				return ii;
@@ -25,21 +26,22 @@ int find_first_index(svec* toks, const char* tt){
 }
 
 
-int contains(svec* toks, const char* tt){
+int 
+contains(svec* toks, const char* tt){
 	return find_first_index(toks, tt) >= 0;
 }
 
 
 svec* 
-slice(svec* toks, int first, int last){
+slice(svec* toks, int start, int end){
 		
-	svec* vector = make_svec();
+	svec* sv = make_svec();
 
-	for(int xx = first; xx < last; ++xx){
-		char* token = svec_get(toks, xx);
-		svec_push_back(vector, token);
+	for(int ii = start; ii < end; ii++){
+		char* token = svec_get(toks, ii);
+		svec_push_back(sv, token);
 	}
-	return vector;	
+	return sv;	
 
 }
 
@@ -47,66 +49,78 @@ slice(svec* toks, int first, int last){
 shell_ast* 
 parse(svec* toks){
 	
-	const char* ops[] = {";", "|", "||", "&", "&&", "<", ">"};
+	const char* operators[] = {";", "|", "||", "&", "&&", "<", ">"};
 
-	int containedOperator = 0;
-	for (int ii= 0; ii < 7; ++ii){
-		const char* operator = ops[ii];
+	int opp = 0;
+	for (int ii= 0; ii < sizeof(operators); ii++){
+		const char* operator = operators[ii];
 		
 		if(contains(toks, operator)){
 
-			//Gettint the index of the first operator 
-			int opIndex = find_first_index(toks, operator);
+			//Get index of first operator 
+			int operatorIndex = find_first_index(toks, operator);
 
-			//Creating operator type node
-			shell_ast* ast = make_shell_ast(); //making node
-			ast->op = svec_get(toks, opIndex); //adding opeator
-			ast->isOperator = 1; //classifying node 
+			//Create operator node
 
-			if(strcmp(operator, ";") == 0 || strcmp(operator, "&&") == 0 || strcmp(operator, "||") == 0){
-				//If the operator is a ; or and AND or and OR we create a left child for the left command and a right child for the right command.
-				//Dividing toks into the right and left part of the operator.
-				svec* left = slice(toks, 0, opIndex);
-				svec* right = slice(toks, opIndex + 1, toks->size);
-				ast->left = parse(left); //adding leftchild 
-				ast->right = parse(right); //adding rightchild
-				free(left); //freeing left child
-				free(right); //freeing right child 
+			//node
+			shell_ast* ast = make_shell_ast(); 
+			//add operator
+			ast->op = svec_get(toks, operatorIndex); 
+			ast->isOperator = 1; 
+
+			//separating commands
+			if(strcmp(operator, ";") == 0 || strcmp(operator, "||") == 0 || strcmp(operator, "&&") == 0){
+				
+				//Divide toks into the right and left branch
+				svec* left = slice(toks, 0, operatorIndex);
+				svec* right = slice(toks, operatorIndex + 1, toks->size);
+				//left child 
+				ast->left = parse(left); 
+				//right child
+				ast->right = parse(right); 
+
+				free(left); 
+				free(right);
 			
-			} else if ( strcmp(operator, "<") == 0 ){
-				//If the operator is redirect input. 
-				ast->cmd = svec_get(toks, 0); //get the command
-				ast->file = svec_get(toks, opIndex + 1); //add the input file
-				ast->args = make_svec(); //creating the argumemtns
-				svec_push_back(ast->args, ast->file); //adding the input file to the arguments 
-				//Know this is not space efficient but will focus on functionality first. 
+			} 
 
-			} else if ( strcmp(operator, ">") == 0) {
-				//If operator is redirect output. 
-				ast->cmd = svec_get(toks, 0); //get the command
-				ast->file = svec_get(toks, opIndex + 1); //add the input file
-				ast->args = make_svec(); //creating arguments
-				for(int ii = 1; ii < toks->size; ++ii){ //adding arguments 
+			//redirect input
+			else if ( strcmp(operator, "<") == 0 ){
+				ast->cmd = svec_get(toks, 0); 
+				ast->file = svec_get(toks, operatorIndex + 1); 
+				//create arguments
+				ast->args = make_svec(); 
+				//push back with input file and args 
+				svec_push_back(ast->args, ast->file);
+			} 
+
+			//redirect output
+			else if ( strcmp(operator, ">") == 0) {
+				ast->cmd = svec_get(toks, 0); 
+				ast->file = svec_get(toks, operatorIndex + 1); 
+				//create arguments
+				ast->args = make_svec();
+				//add arguments
+				for(int ii = 1; ii < toks->size; ii++){  
 					char* token = svec_get(toks, ii);
-					if ( strcmp(token, ">") == 0) break;
+					if ( strcmp(token, ">") == 0)
+						break;
 					svec_push_back(ast->args, token);
 				}
-				//Know this is not space efficient but will focus on functionality first. 
 
-			} else {
+			} 
 
-			}
 			return ast;
 		}
 	}
 
-	if(containedOperator == 0){
+	if(opp == 0){
 		
-		//Creatting command type node
-		shell_ast* ast = make_shell_ast(); //making node
-		ast->cmd = svec_get(toks, 0); //adding command
-		ast->args = make_svec(); //adding command arguments
-		for(int ii = 1; ii < toks->size; ++ii){
+		//Create node
+		shell_ast* ast = make_shell_ast(); 
+		ast->cmd = svec_get(toks, 0); 
+		ast->args = make_svec(); 
+		for(int ii = 1; ii < toks->size; ii++){
 			char* token = svec_get(toks, ii);
 			svec_push_back(ast->args, token);
 		}
@@ -114,7 +128,7 @@ parse(svec* toks){
 		return ast;
 	}
 
-	fprintf(stderr, "Invalid token stream");
+	fprintf(stderr, "Invalid Token Stream");
 	exit(1); 
 		
 
